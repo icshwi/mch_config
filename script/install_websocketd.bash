87637dc2031870331bb94d18ff1a0aea27288b6d
Felipe Torres González <felipe.torresgonzalez@ess.eu>
2020-02-27 10:43:42 +0100

    Added CentOS support for the websocketd installation
    [ICSHWI-4100]

--- script/install_websocketd.bash
+++ script/install_websocketd.bash
@@ -2,23 +2,37 @@
 # Just a simple script to install websocketd
 # https://github.com/joewalnes/websocketd/wiki/Download-and-install
 
-WEBSOCKET_SRC=https://github.com/joewalnes/websocketd/releases/download/v0.3.1/websocketd-0.3.1
+WEBSOCKET_SRC=https://github.com/joewalnes/websocketd/releases/download
+VERSION="0.3.1"
 
 distro=$(cat /etc/os-release | grep -oh "^NAME=.*" | cut -d"=" -f2 | sed 's|\"||g' | cut -d" " -f1)
+
+arch=$(uname -m)
 
 case $distro in
     Ubuntu|Debian)
         pkg_ext=".deb"
+        distro_sep="-"
+        # Websocket developer is an AMD fanboy...
+        arch=$(echo $arch | sed 's|x86_64|amd64|g')
+    ;;
+    CentOS)
+        pkg_ext=".rpm"
+        distro_sep="."
     ;;
     *) echo "OS not supported, sorry!"; exit 1
     ;;
 esac
 
-arch=$(uname -m)
-# Websocket developer is an AMD fanboy...
-arch=$(echo $arch | sed 's|x86_64|amd64|g')
+filename="${WEBSOCKET_SRC}/v${VERSION}/websocketd${distro_sep}${VERSION}${distro_sep}${arch}${pkg_ext}"
+wget $filename
 
-wget ${WEBSOCKET_SRC}_${arch}${pkg_ext}
-sudo dpkg -i websocketd-0.3.1_${arch}${pkg_ext}
-rm websocketd-0.3.1_${arch}${pkg_ext}
+case $distro in
+    Ubuntu|Debian)
+        sudo dpkg -i $filename;;
+    CentOS)
+        sudo rpm -i $filename;;
+esac
 
+rm websocketd*${pkg_ext}
+echo "Websocket succesfully installed!"
